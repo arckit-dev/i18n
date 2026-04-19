49f7e9efac9ab619395641581e30f6493377460d
Marc Gavanier <marc@gavanier.com>
2026-04-20 00:47:01 +0200

    fix: expose resource loaders as subpath exports with sideEffects

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,9 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts',
     client: 'src/client/index.ts',
-    detectors: 'src/detectors/index.ts'
+    detectors: 'src/detectors/index.ts',
+    'resource-loader.node': 'src/resource-loader.node.ts',
+    'resource-loader.fetch': 'src/resource-loader.fetch.ts'
   },
   format: ['esm', 'cjs'],
   dts: true,
