c60f4bb690e8273ce28fe5354d2188d8d2f888b9
Marc Gavanier <marc@gavanier.com>
2026-04-20 20:35:16 +0200

    feat: replace direct injection imports with factory pattern
    - Add createI18n({ inject }) factory that returns configured functions
    - createWithTranslation accepts inject as parameter instead of importing it
    - registerNodeResourceLoader/registerFetchResourceLoader accept provide as parameter
    - Remove @arckit/injection from peer and dev dependencies
    - piqure remains as direct dependency for InjectionKey type and key()

---
+++ src/create-i18n.ts
@@ -0,0 +1,12 @@
+import type { InjectionKey } from 'piqure/src/Providing';
+import { createWithTranslation } from './with-translation';
+
+type Inject = <T>(key: InjectionKey<T>) => T;
+
+export type I18nDependencies = {
+  inject: Inject;
+};
+
+export const createI18n = ({ inject }: I18nDependencies) => ({
+  withTranslation: createWithTranslation(inject)
+});
